51d5fdd17c466b74469354edba0cf1ea9d271afa
melvintj <32409042+melvintj@users.noreply.github.com>
2019-06-09 22:23:18 +0200

    Website edited
    https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php

--- Website/aanmelden.php
+++ Website/aanmelden.php
@@ -1,103 +1,157 @@
-
 <?php
+// Include config file
+require_once "config.php";
 
-include("userValidationClass.php");
-$userClass = new UserClass();
+// Define variables and initialize with empty values
+$username = $password = $confirm_password = "";
+$username_err = $password_err = $confirm_password_err = "";
 
-$errorMessage  = "";
-$sucessMessage = "";
-$email_check = false;
-if (!empty($_POST['submitregistrationform'])) {
-    
-    $email          = $_POST['email'];
-    $password       = $_POST['password'];
-    //$date= date();
-    /* Regular expression check */
-    $email_check    = preg_match('~^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.([a-zA-Z]{2,4})$~i', $email);
-    //$password_check = preg_match('~^[A-Za-z0-9!@#$%^&*()_]{6,20}$~i', $password);
-    if ($email_check) {
-        
-        $uid = $userClass->userRegistration($email, $password);
-        
-        if ($uid) {
-            
-            $sucessMessage = "Registration successful, Please
-   " . "<a href='inloggen.php'>Login</a>";
-            
-        } else {
-            $errorMessage = "Email-ID already exists";
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT UserID FROM portal WHERE Username = :username";
+
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                if($stmt->rowCount() == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
         }
-        
-        
-    } else {
-        $errorMessage = "Please enter the valid details";
-    }
-    
-}
 
+        // Close statement
+        unset($stmt);
+    }
+
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+
+        // Prepare an insert statement
+        $sql = "INSERT INTO portal (Username, Password) VALUES (:username, :password)";
+
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            $stmt->bindParam(":password", $param_password, PDO::PARAM_STR);
+
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Redirect to login page
+                header("location: inloggen.php");
+            } else{
+                echo "Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        unset($stmt);
+    }
+
+    // Close connection
+    unset($pdo);
+}
 ?>
 
+    <!DOCTYPE html>
+    <html>
 
-<!--
-BOVENSTAAND = PHP ONLY
-ONDERSTAAND = HTML ONLY
--->
+    <head>
+        <meta charset="UTF-8">
+        <title>Super Thuiszorg</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
 
-<!DOCTYPE html>
-<html>
-   <head>
-      <meta charset="UTF-8">
-      <title>Super Thuiszorg</title>
-      <link rel="stylesheet" type="text/css" href="style.css">
     </head>
-   <body>
-    <div class="topnav">
-        <img class="logo" src="Afbeeldingen/logo.jpg" alt="Super Thuiszorg logo" align="left">
-        <a href="index.html">Home</a>
-        <a href="werkwijze.html">Werkwijze</a>
-        <a href="overons.html">Over ons</a>
-        <a href="contact.html">Contact</a>
-        <a href="inloggen.php">Inloggen</a>
-        <a class="active" href="aanmelden.html">Aanmelden</a>
-    </div>
-      <br>
-       <h1>Aanmelden</h1>
-         
-         <div class="container">
+
+    <body>
+        <div class="topnav">
+            <img class="logo" src="Afbeeldingen/logo.jpg" alt="Super Thuiszorg logo" align="left">
+            <a href="index.html">Home</a>
+            <a href="werkwijze.html">Werkwijze</a>
+            <a href="overons.html">Over ons</a>
+            <a href="contact.html">Contact</a>
+            <a href="inloggen.php">Inloggen</a>
+            <a class="active" href="aanmelden.php">Aanmelden</a>
+        </div>
+
+        <br>
+        <h1>Aanmelden</h1>
+
+        <div class="container">
             <h2>Voor het aanmelden van onze dienstverlening moet u een account aanmaken:</h2>
             <hr>
-            <div>
-               <table>
-                   <form method="post" action="" name="register">
-                      <tr>
-                        <th>
-                                <label><b>Email</b></label><br>
-                        </th>
-                            <th><input type="text" name="email" autocomple="off" required></th>
-                        </tr>
-                        <tr>
-                         <th>
-                            <label><b>Wachtwoord</b></label><br>
-                        </th>
-                        <th>
-                            <input type="password" name="password" autocomple="off" required>
-                            <div class="errorMsg"><?php echo $errorMessage; ?></div>
-                            <div class="sucessMsg"><?php echo $sucessMessage; ?></div>
-                            <input type="submit" class="button" name="submitregistrationform" value="Register">
-                         </th>
-                      </tr>
-                    </form>
-               </table>
-            </div>  
-         </div>
-          &nbsp;Bij het aanmaken van een account gaat u akkoord met onze <a href="#">Terms & Privacy</a><br>
-      <div>
-          &nbsp;Heeft u al een account? <a href="inloggen.html">Inloggen</a>
-      </div>
-      <div class="copyright">
-         <p>
-            © Copyright by Super Thuiszorg!
-         </p>
-      </div>
-   </body>
-</html>
+            <div class="wrapper">
+                <h2>Sign Up</h2>
+                <p>Please fill this form to create an account.</p>
+                <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]); ?>" method="post">
+                    <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                        <label>Username</label>
+                        <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                        <span class="help-block"><?php echo $username_err; ?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                        <label>Password</label>
+                        <input type="password" name="password" class="form-control" value="<?php echo $password; ?>">
+                        <span class="help-block"><?php echo $password_err; ?></span>
+                    </div>
+                    <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                        <label>Confirm Password</label>
+                        <input type="password" name="confirm_password" class="form-control" value="<?php echo $confirm_password; ?>">
+                        <span class="help-block"><?php echo $confirm_password_err; ?></span>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Submit">
+<!--                        <input type="reset" class="btn btn-default" value="Reset">-->
+                    </div>
+                    <p>Already have an account? <a href="inloggen.php">Login here</a>.</p>
+                </form>
+            </div>
+        </div>
+        &nbsp;Bij het aanmaken van een account gaat u akkoord met onze <a href="#">Terms & Privacy</a>
+        <br>
+        <div class="copyright">
+            <p>
+                © Copyright by Super Thuiszorg!
+            </p>
+        </div>
+    </body>
+
+    </html>
--- Website/inloggen.php
+++ Website/inloggen.php
@@ -1,40 +1,90 @@
 <?php
-
-include("userValidationClass.php");
-$userClass = new UserClass();
-
-$errorMessage = "";
-
-if (!empty($_POST['submitloginform'])) {
-    
-    $email    = $_POST['email'];
-    $password = $_POST['password'];
-    
-    if (strlen(trim($email)) > 1 && strlen(trim($password)) > 1) {
-        
-        $uid = $userClass->userLogin($email, $password);
-        if ($uid) {
-            $url = 'index.php';
-            header("Location: $url"); // Page redirecting to home.php 
-        } else {
-            $errorMessage = "Please enter the valid credential";
-        }
-        
-    } else {
-        $errorMessage = "Please Enter the valid details";
-        
+// Initialize the session
+session_start();
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: welcome.php");
+    exit;
+}
+ 
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = "";
+$username_err = $password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    // Check if username is empty
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter username.";
+    } else{
+        $username = trim($_POST["username"]);
     }
     
+    // Check if password is empty
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter your password.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
     
+    // Validate credentials
+    if(empty($username_err) && empty($password_err)){
+        // Prepare a select statement
+        $sql = "SELECT id, username, password FROM users WHERE username = :username";
+        
+        if($stmt = $pdo->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bindParam(":username", $param_username, PDO::PARAM_STR);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Check if username exists, if yes then verify password
+                if($stmt->rowCount() == 1){
+                    if($row = $stmt->fetch()){
+                        $id = $row["id"];
+                        $username = $row["username"];
+                        $hashed_password = $row["password"];
+                        if(password_verify($password, $hashed_password)){
+                            // Password is correct, so start a new session
+                            session_start();
+                            
+                            // Store data in session variables
+                            $_SESSION["loggedin"] = true;
+                            $_SESSION["id"] = $id;
+                            $_SESSION["username"] = $username;                            
+                            
+                            // Redirect user to welcome page
+                            header("location: welcome.php");
+                        } else{
+                            // Display an error message if password is not valid
+                            $password_err = "The password you entered was not valid.";
+                        }
+                    }
+                } else{
+                    // Display an error message if username doesn't exist
+                    $username_err = "No account found with that username.";
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+        
+        // Close statement
+        unset($stmt);
+    }
+    
+    // Close connection
+    unset($pdo);
 }
 ?>
-
-
-<!--
-BOVENSTAAND = PHP ONLY
-ONDERSTAAND = HTML ONLY
--->
-
 
 <!DOCTYPE html>
 <html>
@@ -42,6 +92,7 @@ ONDERSTAAND = HTML ONLY
       <meta charset="UTF-8">
       <title>Super Thuiszorg</title>
       <link rel="stylesheet" type="text/css" href="style.css">
+       
     </head>
    <body>
     <div class="topnav">
@@ -54,29 +105,26 @@ ONDERSTAAND = HTML ONLY
         <a href="aanmelden.php">Aanmelden</a>
     </div>
       <br>
-      <h1>Inloggen</h1>
-      <form action="userValidationClass.php">
-         <div class="container">
-            <h2>Voor het inloggen met u account vult u de onderstaande gegevens in:</h2>
-            <hr>
-             
-            <table>
-                <form method="post" action="" name="login">
-                   <tr>
-                      <th><label><b>E-mail:</b></label></th>
-                      <th><input type="text" placeholder="e-mail...." name="email" required><br></th>
-                   </tr>
-                   <tr>
-                        <th><label><b>Wachtwoord</b></label></th>
-                        <th><input type="password" placeholder="wachtwoord...." name="password" required></th>
-                        <div class="errorMsg"><?php echo $errorMessage; ?></div>
-                   </tr>
-                </form>  
-            </table>
-            <input type="submit" class="button" name="submitloginform" value="Login">
-         </div>
-            <span class="psw"><a href="#">Wachtwoord vergeten?</a></span>
-      </form>
+      <div class="wrapper">
+        <h2>Login</h2>
+        <p>Please fill in your credentials to login.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control" value="<?php echo $username; ?>">
+                <span class="help-block"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control">
+                <span class="help-block"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Login">
+            </div>
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>
        <br><br><br><br>   
        <div class="copyright">
          <p>
