c1a9d60c6bb041885d1cc9b03531c9b020a2b432
melvintj <32409042+melvintj@users.noreply.github.com>
2019-06-12 21:49:42 +0200

    Uitloggen #inprogess

---
+++ Website/uitloggen.php
@@ -0,0 +1,10 @@
+<?php
+
+    session_destroy();
+    header("location: index.html");
+//        unset($_SESSION["loggedin"]);
+//        unset($_SESSION["UserID"]);
+//        unset($_SESSION["username"]); 
+
+        
+?>
