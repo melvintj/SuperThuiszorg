b7a99d9ceb27167a10a9fb79fdb67892168fd799
melvintj <32409042+melvintj@users.noreply.github.com>
2019-06-14 11:26:45 +0200

    logout #fixed

--- Website/uitloggen.php
+++ Website/uitloggen.php
@@ -1,10 +1,19 @@
 <?php
+//    header("location: inloggen.php");
+//        session_unset();
+//        session_destroy();
+//        session_write_close();
+//        unset($_SESSION['loggedin']);
+//        unset($_SESSION['UserID']);
+//        unset($_SESSION['username']); 
 
+    header("location: inloggen.php");
+    session_start();
+    session_unset();
     session_destroy();
-    header("location: index.html");
-//        unset($_SESSION["loggedin"]);
-//        unset($_SESSION["UserID"]);
-//        unset($_SESSION["username"]); 
-
-        
+    session_write_close();
+    setcookie(session_name(),'',0,'/');
+    session_regenerate_id(true);
 ?>
+
+
