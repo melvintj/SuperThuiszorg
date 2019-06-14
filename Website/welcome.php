63f8f9d9ac5bc8f5535eaa0eca3197585545cfef
melvintj <32409042+melvintj@users.noreply.github.com>
2019-06-14 11:36:26 +0200

    contact.html edited to contact.php

--- Website/welcome.php
+++ Website/welcome.php
@@ -20,7 +20,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         <a href="index.html">Home</a>
         <a href="werkwijze.html">Werkwijze</a>
         <a href="overons.html">Over ons</a>
-        <a href="contact.html">Contact</a>
+        <a href="contact.php">Contact</a>
         <a class="active" href="inloggen.php">Inloggen</a>
         <a href="aanmelden.php">Aanmelden</a>
     </div>
